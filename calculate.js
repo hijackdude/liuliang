445df443e34fdbae8c7e2410ba375645aef9e6da
hijackdude <1399128254@qq.com>
2019-05-09 15:59:27 +0800

    a
    a'a

---
+++ calculate.js
@@ -0,0 +1,81 @@
+$(document).ready( function () {
+
+
+    // VARIABLES
+    // ----------------------------------------------------------
+
+    var amount, percent, result;
+    var calculator = $('.calculator');
+    var calculatorBill = calculator.find('.calculator__bill');
+    var calculatorTip = calculator.find('.calculator__tip');
+    var calculatorTip2 = calculator.find('.calculator__tip2');
+    var calculatorResult = calculator.find('.calculator__result');
+    var billamount = calculator.find('.bill-amount');
+    var tipAmount = calculator.find('.tip-amount');
+    var tipAmount2 = calculator.find('.tip2-amount');
+
+    // INIT BILL
+    // ----------------------------------------------------------
+
+    $(window).on('DOMContentLoaded', function () {
+        billamount.text(calculatorBill.val())
+        tipAmount.text( calculatorTip.val() );
+        tipAmount2.text( calculatorTip2.val() );
+        amount = calculatorBill.val() * 1;
+        percent = calculatorTip.val() * 1;
+        percent2 = calculatorTip2.val() * 1;
+        result = amount + amount * ( percent / 100 ) + percent2;
+        calculatorResult.text( result.toFixed(2)  );
+    });
+
+
+    // RANGE FUNCTION
+    // ----------------------------------------------------------
+
+    calculatorBill.on('change', function () {
+
+        if ( calculatorBill.val() === '' || isNaN( calculatorBill.val() ) ) {
+            alert('Enter bill amount, please!')
+        } else {
+            amount = calculatorBill.val() * 1;
+        }
+        billamount.text( calculatorBill.val());
+        amount = calculatorBill.val() * 1;
+        percent = calculatorTip.val() * 1;
+        percent2 = calculatorTip2.val() * 1;
+        result = amount * 0.015 + percent * 0.02 + percent2 *0.03;
+        calculatorResult.text( result.toFixed(2)  );
+    });
+
+
+    calculatorTip2.on('change', function () {
+
+    if ( calculatorBill.val() === '' || isNaN( calculatorBill.val() ) ) {
+        alert('Enter bill amount, please!')
+    } else {
+        amount = calculatorBill.val() * 1;
+    }
+
+    tipAmount2.text( calculatorTip2.val());
+    percent2 = calculatorTip2.val() * 1;
+    result = amount * 0.015 + percent * 0.02 + percent2 *0.03;
+    calculatorResult.text( result.toFixed(2) );
+});
+
+
+    calculatorTip.on('change', function () {
+
+        if ( calculatorBill.val() === '' || isNaN( calculatorBill.val() ) ) {
+            alert('Enter bill amount, please!')
+        } else {
+            amount = calculatorBill.val() * 1;
+        }
+
+        tipAmount.text( calculatorTip.val());
+        percent = calculatorTip.val() * 1;
+        result = amount * 0.015 + percent * 0.02 + percent2 *0.03;
+        calculatorResult.text( result.toFixed(2) );
+    });
+
+});
+
